b128119558498cda74c1acdb58710293b8c04c49
acontell <acontell@localhost.localdomain>
2018-02-18 12:34:02 +0100

    - Added system tests sample

---
+++ bruuser-st/src/test/java/com/bruuser/business/appuser/boundary/AppUsersResource.java
@@ -0,0 +1,33 @@
+package com.bruuser.business.appuser.boundary;
+
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
+import javax.json.JsonArray;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class AppUsersResource {
+    private static final String URI = "http://localhost:8080/bruuser/resources/appuser";
+    @Rule
+    public JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI(URI);
+    
+    @Test
+    public void getShouldWithMediaTypeJsonShouldBeSuccessfull() {
+        Response response = this.provider.target().request(MediaType.APPLICATION_JSON).get();
+        assertThat(response.getStatus(), is(200));
+    }
+    
+    @Test
+    public void getShouldWithMediaTypeJsonShouldReturnListOfUsers() {
+        // Depends on the state of the server
+        Response response = this.provider.target().request(MediaType.APPLICATION_JSON).get();
+        JsonArray allUsers = response.readEntity(JsonArray.class);
+        assertTrue(allUsers.isEmpty());
+    }
+    
+    // More tests can be added to perform system tests over bruuser
+}
